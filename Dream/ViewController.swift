9ba9a39b1d5149b24c7f87226affd355ca0ab1e9
denism <demaltsev@gmail.com>
2019-10-08 02:00:21 +0300

    Initial Commit

---
+++ Dream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dream
+//
+//  Created by denis on 08/10/2019.
+//  Copyright © 2019 Unknown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
